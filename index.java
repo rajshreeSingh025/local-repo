a8f9b2009e4b3a106de74f5670288d926d78c365
Rajshree singh <singhrajshree1001@gmail.com>
2025-02-19 10:18:02 +0530

    new feature1

--- index.java
+++ index.java
@@ -3,5 +3,6 @@ package localRepo;
 public class index {
     public static void main(String[] args) {
         System.out.println("this is my repo");
+        System.out.println("this is the new feature");
     }
 }
